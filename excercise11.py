191ae50018ff15a13c9de36683717d6f6a3e8b7e
Mahlori-Baloyi <69038017+Mahlori-Baloyi@users.noreply.github.com>
2020-10-12 23:43:13 +0200

    Update excercise11.py

--- excercise11.py
+++ excercise11.py
@@ -1,10 +1,5 @@
-string_1 = input("input first string: ")
-string_2 = input("input second string: ")
-
-def CommonCharactrs(string_1,string_2):
-    my_str = " "
-    for i in set(string_1):
-        if i in string_2:
-            my_str += i
-    return  my_str
-print(CommonCharactrs(string_1,string_2))
+def CheckVowels(words):
+    for i in words:
+        if i in "AaEeIiOoUu":
+            print(i)
+CheckVowels("UmUzi AcadEmy")
