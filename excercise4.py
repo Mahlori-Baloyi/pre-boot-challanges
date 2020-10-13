bb242d961766f1d6b570882b98e360b4d5ebb67d
Mahlori-Baloyi <69038017+Mahlori-Baloyi@users.noreply.github.com>
2020-10-13 15:34:13 +0200

    Update excercise4.py

--- excercise4.py
+++ excercise4.py
@@ -1,13 +1,11 @@
 a = int(input("enter the first variable: "))
 b = int(input("enter the second variable: "))
 def CheckIfContains3(a,b):
-    sum = a + b
+    c = a + b
 
-    if a == 3 or b == 3:
+    if ("3" in str(c)) and (a == 3 or b == 3):
         return "true"
-    if ("3" in str(sum)):
-        return "true"
+
     else:
         return "false"
-
 print(CheckIfContains3(a,b))
